095d79a2c71ce6ecc74c32f2c9d4fbb21f08c3df
Kovács Gergely <gergo.kovacs@innonic.com>
2016-11-02 16:23:21 +0100

    START

---
+++ test/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kovacsgergely
+ * Date: 2016.11.02.
+ * Time: 15:49
+ */
+
+class SampleClassTest extends PHPUnit_Framework_TestCase
+{
+    public function testSampleFunction()
+    {
+        self::assertTrue(true);
+    }
+}
